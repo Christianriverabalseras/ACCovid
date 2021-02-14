388747154251c0172e1793072c3c5644173f25cb
melaniedura <melanie.159.dg@gmail.com>
2021-02-14 15:45:02 +0100

    Fix merge conflicts. Update material to 1.3.0. Remove kotlin android extensions plugin.

--- app/src/main/java/com/ac03/covid/data/server/CovidService.kt
+++ app/src/main/java/com/ac03/covid/data/server/CovidService.kt
@@ -1,8 +1,12 @@
 package com.ac03.covid.data.server
 
+import com.ac03.covid.entites.Countries
 import retrofit2.http.GET
 
 interface CovidService {
     @GET("/summary")
     suspend fun getSummary(): SummaryData
+
+    @GET("/countries")
+    suspend fun getCountries(): Countries
 }
--- app/src/main/java/com/ac03/covid/ui/HomeFragment.kt
+++ app/src/main/java/com/ac03/covid/ui/HomeFragment.kt
@@ -1,19 +1,18 @@
 package com.ac03.covid.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import androidx.fragment.app.Fragment
 import com.ac03.covid.R
+import com.ac03.covid.data.server.CovidServiceFactory
 import com.ac03.covid.databinding.FragmentHomeBinding
-import com.ac03.covid.api.CovidApiFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlin.collections.ArrayList
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
@@ -21,9 +20,10 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -37,7 +37,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private suspend fun getCountries() {
-        val countries = CovidApiFactory.service.getCountries()
+        val countries = CovidServiceFactory.service.getCountries()
         val countriesNames: ArrayList<String> = arrayListOf()
         countries.forEach { countryItem -> countriesNames.add(countryItem.country) }
         val adapter = ArrayAdapter(requireContext(), R.layout.list_item_sp_menu, countriesNames.sorted())
@@ -45,7 +45,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private suspend fun globalData() {
-        val summary = CovidApiFactory.service.getSummary()
+        val summary = CovidServiceFactory.service.getSummary()
         binding.globalConfirmed.text = "Total confirmados: ${ summary.global.totalConfirmed }"
         binding.globalDeath.text = "Total muertes: ${ summary.global.totalDeaths }"
         binding.globalRecovered.text = "Total recuperados: ${ summary.global.totalRecovered }"
@@ -55,5 +55,4 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onDestroyView()
         _binding = null
     }
-
 }
