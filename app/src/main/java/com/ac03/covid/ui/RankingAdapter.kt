056ac46e3287c4ad86a6e6cd3c58ad15285578fa
melaniedura <melanie.159.dg@gmail.com>
2021-03-28 15:31:47 +0200

    fix recycler view

--- app/src/main/java/com/ac03/covid/ui/RankingAdapter.kt
+++ app/src/main/java/com/ac03/covid/ui/RankingAdapter.kt
@@ -1,46 +1,34 @@
 package com.ac03.covid.ui
 
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.ac03.covid.R
 import com.ac03.covid.databinding.ItemCountryBinding
 import com.ac03.covid.model.server.Country
 import com.ac03.covid.ui.RankingAdapter.ViewHolder
 import com.ac03.covid.util.viewBinding
 
-class RankingAdapter(private val countries: (Country) -> Unit) :
-    ListAdapter<Country, RankingAdapter.ViewHolder>(DiffUtilCallback) {
+// TODO: aquí no necesitas pasarle la list de paises en el constructor, ya que eso lo hacemos con
+// el método submitList en el fragment
+class RankingAdapter : ListAdapter<Country, ViewHolder>(DiffUtilCallback) {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.item_country, parent, false)
-        return ViewHolder(view)
-    }
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-//        ViewHolder(viewBinding(parent, ItemCountryBinding::inflate))
+    // TODO: esto estaba bien, lo único que te faltaba era al ViewHolder pasarle el binding, en lugar de la vista
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(viewBinding(parent, ItemCountryBinding::inflate))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val country = getItem(position)
         holder.bind(country)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val binding = ItemCountryBinding.bind(view)
-
+    class ViewHolder(private val binding: ItemCountryBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(country: Country) = with(binding) {
-            this.countryItem.text = country.country
-            this.confirmedItem.text = country.totalConfirmed.toString()
-            this.deathItem.text = country.totalDeaths.toString()
-            this.recoveredItem.text = country.totalRecovered.toString()
+            countryItem.text = country.country
+            confirmedItem.text = country.totalConfirmed.toString()
+            deathItem.text = country.totalDeaths.toString()
+            recoveredItem.text = country.totalRecovered.toString()
         }
-
     }
 }
 
@@ -52,5 +40,4 @@ private object DiffUtilCallback : DiffUtil.ItemCallback<Country>() {
     override fun areContentsTheSame(oldItem: Country, newItem: Country): Boolean {
         return oldItem == newItem
     }
-
 }
--- app/src/main/java/com/ac03/covid/ui/StatisticsFragment.kt
+++ app/src/main/java/com/ac03/covid/ui/StatisticsFragment.kt
@@ -4,42 +4,36 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ac03.covid.R
 import com.ac03.covid.databinding.FragmentStatisticsBinding
 import com.ac03.covid.model.server.CovidRepository
-import com.ac03.covid.util.viewBinding
-import androidx.lifecycle.Observer
-import com.ac03.covid.model.server.Country
 import com.ac03.covid.ui.StatisticsViewModel.UiModel.*
-import kotlinx.coroutines.delay
+import com.ac03.covid.util.viewBinding
 
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     private val binding by viewBinding(FragmentStatisticsBinding::bind)
     private lateinit var viewModel: StatisticsViewModel
-//    private val viewModel by viewModels<StatisticsViewModel>()
 
-    private lateinit var adapter: RankingAdapter
-
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        viewModel = getViewModel { StatisticsViewModel(CovidRepository(requireActivity().app)) }
-//
-//        viewModel.model.observe(viewLifecycleOwner, Observer(::updateUi))
-//        val recycler = binding.countryRecyclerView
-//        val results = CovidRepository.findCountries
-//
-//        recycler.adapter = RankingAdapter(listOf())
-//    }
+    private val rankingAdapter by lazy { RankingAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = getViewModel { StatisticsViewModel(CovidRepository(requireActivity().app)) }
         viewModel.model.observe(viewLifecycleOwner, Observer(::updateUi))
+        setupList()
     }
+
+    private fun setupList() = with(binding.countryRecyclerView) {
+        // TODO: realmente, solo te faltaba el asignar el layoutManager al recycler view,
+        // y sobraba el pasarle una lista vacía al inicializar el adapter.
+        adapter = rankingAdapter
+        layoutManager = LinearLayoutManager(context)
+    }
+
     private fun updateUi(model: StatisticsViewModel.UiModel) {
         when (model) {
             is Loading -> TODO()
@@ -51,14 +45,12 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
     private fun showRecycler(model: Content) {
         val results = model.data.countries
 
-        var newResults = listOf<Country>()
-        for (country in results) {
-            newResults = listOf(country) + newResults
-        }
-//        binding.countryRecyclerView.adapter = RankingAdapter(newResults)
-//        binding.countryRecyclerView.adapter.submitList(newResults)
-        adapter.submitList(newResults)
+        // TODO: Cuál es la finalidad de esto?
+//        var newResults = listOf<Country>()
+//        for (country in results) {
+//            newResults = listOf(country) + newResults
+//        }
+
+        rankingAdapter.submitList(results)
     }
-
-
 }
